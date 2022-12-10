8231cd064384af2e0b40faee6162121bc0eae04e
Pavel <pavel07007@gmail.com>
2022-12-10 09:44:16 +0300

    refactoring and change font

--- src/Tablo.tsx
+++ src/Tablo.tsx
@@ -1,55 +1,57 @@
-import React, {ChangeEvent, useState,KeyboardEvent} from 'react';
-import s from  './Tablo.module.css'
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
+import s from './Tablo.module.css'
 import Button from "./Button";
+
 const Tablo = () => {
     const [message, setMessage] = useState<Array<string>>([])
 
     const [title, setTitle] = useState('')
     const [error, setError] = useState('')
 
-    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>)=>{
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
-        if(message.length<5){
+        if (message.length < 5) {
             setError('')
         }
     }
-    const addMessage = ()=>{
-        if(title.trim() === ''){
+    const addMessage = () => {
+        if (title.trim() === '') {
             setTitle('')
             setError('Wrong Value!')
             return;
         }
-        if(message.length<5){
-        message.push(title)
-        setMessage([...message])
-        setTitle('')}
-    else{
-        setError('Limit of messages is exceeded.')
-        return setTitle('')
-    }}
-    const deleteMessage = ()=>{
-        setMessage( [])
+        if (message.length < 5) {
+            message.push(title)
+            setMessage([...message])
+            setTitle('')
+        } else {
+            setError('Limit of messages is exceeded.')
+            return setTitle('')
+        }
     }
-    const deleteLastMessage = ()=>{
+    const deleteMessage = () => {
+        setMessage([])
+    }
+    const deleteLastMessage = () => {
         let x = message.pop()
-        setMessage(message.slice(0,message.length))
+        setMessage(message.slice(0, message.length))
         setError('')
     }
-    const onEnterAdd = (e:KeyboardEvent<HTMLInputElement>)=>{
-        if(e.key === "Enter"){
+    const onEnterAdd = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
             addMessage()
         }
     }
-    const mappedMessage = message.map(el=> <li>{el}</li>)
+    const mappedMessage = message.map(el => <li>{el}</li>)
     return (
         <div className={s.tablo}>
             {error && <p className={s.red}>{error}</p>}
             <div className={s.input_with_button}>
-            <input onKeyDown={onEnterAdd} value={title} onChange={onChangeHandler} type="text"/>
+                <input onKeyDown={onEnterAdd} value={title} onChange={onChangeHandler} type="text"/>
                 <Button name={'send'} callback={addMessage}/>
-                <Button name={'clear'} callback={deleteMessage} />
+                <Button name={'clear'} callback={deleteMessage}/>
             </div>
-            <Button callback={deleteLastMessage} name={'Delete last message'} disabled={false} />
+            <Button callback={deleteLastMessage} name={'Delete last message'} disabled={false}/>
             <ul>{mappedMessage}</ul>
         </div>
     );
